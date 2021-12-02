469ed9d69449a64165cc5afcad86cec433125b05
damiandattilo1 <damiandattilo@yahoo.com.ar>
2021-12-02 20:51:50 -0300

    Recuperatorio TP3

---
+++ TP3 Dattilo Damian Comision 2A/IngresoProductos/FrmIngresoProductos.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace IngresoProductos
+{
+    partial class FrmIngresoProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnVerLista = new System.Windows.Forms.Button();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.cmbCaracteristica = new System.Windows.Forms.ComboBox();
+            this.cmbEficiencia = new System.Windows.Forms.ComboBox();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(272, 362);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(276, 80);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar Producto";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnVerLista
+            // 
+            this.btnVerLista.Location = new System.Drawing.Point(806, 362);
+            this.btnVerLista.Name = "btnVerLista";
+            this.btnVerLista.Size = new System.Drawing.Size(262, 80);
+            this.btnVerLista.TabIndex = 1;
+            this.btnVerLista.Text = "Ver lista de productos";
+            this.btnVerLista.UseVisualStyleBackColor = true;
+            this.btnVerLista.Click += new System.EventHandler(this.btnVerLista_Click);
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(618, 63);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(151, 28);
+            this.cmbProducto.TabIndex = 2;
+            this.cmbProducto.SelectedIndexChanged += new System.EventHandler(this.cmbProducto_SelectedIndexChanged);
+            // 
+            // cmbCaracteristica
+            // 
+            this.cmbCaracteristica.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCaracteristica.FormattingEnabled = true;
+            this.cmbCaracteristica.Location = new System.Drawing.Point(894, 206);
+            this.cmbCaracteristica.Name = "cmbCaracteristica";
+            this.cmbCaracteristica.Size = new System.Drawing.Size(151, 28);
+            this.cmbCaracteristica.TabIndex = 3;
+            // 
+            // cmbEficiencia
+            // 
+            this.cmbEficiencia.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEficiencia.FormattingEnabled = true;
+            this.cmbEficiencia.Location = new System.Drawing.Point(484, 206);
+            this.cmbEficiencia.Name = "cmbEficiencia";
+            this.cmbEficiencia.Size = new System.Drawing.Size(151, 28);
+            this.cmbEficiencia.TabIndex = 4;
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(249, 206);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(151, 28);
+            this.cmbMarca.TabIndex = 5;
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Location = new System.Drawing.Point(706, 207);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(125, 27);
+            this.txtPorcentaje.TabIndex = 6;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(1145, 206);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(125, 27);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(53, 207);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(125, 27);
+            this.txtId.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(618, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Elija tipo de producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Marca Del Producto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(475, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Eficiencia Energetica";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(693, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Porcentaje de fallas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(917, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "caracteristica";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1188, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "precio";
+            // 
+            // FrmIngresoProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 485);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.cmbMarca);
+            this.Controls.Add(this.cmbEficiencia);
+            this.Controls.Add(this.cmbCaracteristica);
+            this.Controls.Add(this.cmbProducto);
+            this.Controls.Add(this.btnVerLista);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "FrmIngresoProductos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmIngresoProductos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnVerLista;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.ComboBox cmbCaracteristica;
+        private System.Windows.Forms.ComboBox cmbEficiencia;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ TP3 Dattilo Damian Comision 2A/IngresoProductos/FrmIngresoProductos.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Entidades;
+
+namespace IngresoProductos
+{
+    public partial class FrmIngresoProductos : Form
+    {
+        public Taller<Producto> listaProductos;
+        public int capacidadMaxima;
+        public FrmIngresoProductos()
+        {
+            InitializeComponent();
+
+            this.cmbProducto.Items.AddRange(new String[] { "Microondas", "Aire Acondicionado" });
+
+            this.cmbEficiencia.Items.AddRange(new String[] { "A", "B", "C", "D" });
+
+            this.cmbMarca.Items.AddRange(new String[] { "LG", "Whirlpool", "Philco", "Samsung", "Sony" });
+
+            cmbProducto.Text = ("Ingrese un producto");
+
+            capacidadMaxima = 7;
+            listaProductos = new Taller<Producto>(capacidadMaxima);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            if (cmbProducto.Text == "Aire Acondicionado")
+            {
+                EMarca m = (EMarca)Enum.Parse(typeof(EMarca), cmbMarca.Text);
+                EficienciaEnergetica eE = (EficienciaEnergetica)Enum.Parse(typeof(EficienciaEnergetica), cmbEficiencia.Text);
+                ETipo t = (ETipo)Enum.Parse(typeof(ETipo), cmbCaracteristica.Text);
+
+                AireAcondicionado ac = new AireAcondicionado(Int32.Parse(txtId.Text), m, eE, Int32.Parse(txtPorcentaje.Text), t, Int32.Parse(txtPrecio.Text));
+                if (!listaProductos.Agregar(ac))
+                {
+                    if (listaProductos.lista.Count == capacidadMaxima)
+                    {
+                        MessageBox.Show("Taller completo!!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("El producto ya se encuentra en la lista!!!");
+                    }
+                }
+
+            }
+            if (cmbProducto.Text == "Microondas")
+            {
+                EMarca m = (EMarca)Enum.Parse(typeof(EMarca), cmbMarca.Text);
+                EficienciaEnergetica eE = (EficienciaEnergetica)Enum.Parse(typeof(EficienciaEnergetica), cmbEficiencia.Text);
+                ETamanio t = (ETamanio)Enum.Parse(typeof(ETamanio), cmbCaracteristica.Text);
+
+                Microondas mi = new Microondas(Int32.Parse(txtId.Text), m, eE, Int32.Parse(txtPorcentaje.Text), t, Int32.Parse(txtPrecio.Text));
+
+                if (!listaProductos.Agregar(mi))
+                {
+                    if (listaProductos.lista.Count == capacidadMaxima)
+                    {
+                        MessageBox.Show("Taller completo!!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("El producto ya se encuentra en la lista!!!");
+                    }
+                }
+            }
+
+
+        }
+
+        private void cmbProducto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.cmbCaracteristica.Items.Clear();
+            if (cmbProducto.Text == "Aire Acondicionado")
+                this.cmbCaracteristica.Items.AddRange(new String[] { "Frio", "Calor", "FrioCalor", "Deshumidificador" });
+            else if (cmbProducto.Text == "Microondas")
+                this.cmbCaracteristica.Items.AddRange(new String[] { " Chico", "Mediano", "Grande" });
+            else
+                cmbProducto.Text = ("Ingrese un producto");
+        }
+
+        private void btnVerLista_Click(object sender, EventArgs e)
+        {
+            FrmMostrarDatos d = new FrmMostrarDatos(listaProductos);
+            d.ShowDialog();
+
+        }
+    }
+}
